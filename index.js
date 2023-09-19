91081d9425d375e2e2bdd133cc71ce919e357a9a
Alin <at.silcock@gmail.com>
2023-09-19 11:57:16 -0600

    lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+ let num1 = 1;
+let num2 = 62;
+var multiply = num1 * num2;
 
-// Write your code here
+var random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 24
+let num4 = 5
+var mod = num3 % num4
+
+var max = Math.max(2,3,6,8,20)
